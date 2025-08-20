97b3fb2ce75150d6ccc52b8df5dedef8cc2f0920
Diego <diegosantos4957@gmail.com>
2025-08-20 19:15:30 -0300

    refactor session use case to return user entity and use repository interface

--- usecases/session.usecase.go
+++ usecases/session.usecase.go
@@ -1,16 +1,30 @@
 package usecases
 
+import (
+	"bit_score/entity"
+	"bit_score/repositories"
+)
+
 type SessionUseCase interface {
-	Login(username string, password string) (string, error)
+	Login(username string, password string) (entity.Users, error)
 }
 
 type sessionUseCase struct {
+	repository repositories.UsersRepositoryInterface
 }
 
-func NewSessionUseCase() SessionUseCase {
-	return &sessionUseCase{}
+func NewSessionUseCase(repository repositories.UsersRepositoryInterface) SessionUseCase {
+	return &sessionUseCase{
+		repository: repository,
+	}
 }
 
-func (s *sessionUseCase) Login(username string, password string) (string, error) {
-	return "Success", nil
+func (s *sessionUseCase) Login(username string, password string) (entity.Users, error) {
+	user, err := s.repository.GetByUserName(username)
+
+	if err != nil {
+		return entity.Users{}, err
+	}
+
+	return user, nil
 }
