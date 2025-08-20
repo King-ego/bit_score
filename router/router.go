e9fa8a3025ed1c8bddd010aef2d450926b6c613d
Diego <diegosantos4957@gmail.com>
2025-08-20 19:23:31 -0300

    update router to accept database connection and pass it to session routes

--- router/router.go
+++ router/router.go
@@ -4,27 +4,30 @@ import (
 	"bit_score/routers"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type SetupRoutes struct {
 	server *gin.Engine
+	db     *mongo.Database
 }
 
-func NewRouter(server *gin.Engine) *SetupRoutes {
+func NewRouter(server *gin.Engine, db *mongo.Database) *SetupRoutes {
 	return &SetupRoutes{
 		server: server,
+		db:     db,
 	}
 }
 
 func (r *SetupRoutes) setupRouters() {
-	routers.SetupSessionRoutes(r.server)
+	routers.SetupSessionRoutes(r.server, r.db)
 }
 
 func (r *SetupRoutes) Routers() {
 	r.setupRouters()
 }
 
-func SetupAllRoutes(server *gin.Engine) {
-	router := NewRouter(server)
+func SetupAllRoutes(server *gin.Engine, db *mongo.Database) {
+	router := NewRouter(server, db)
 	router.Routers()
 }
