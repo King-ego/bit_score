b4f21e2b947653cd170b2aa24a817e7d44a31308
Diego <diegosantos4957@gmail.com>
2025-08-20 19:19:59 -0300

    refactor sessions controller to inject session use case and handle user login

--- controllers/sessions.controller.go
+++ controllers/sessions.controller.go
@@ -2,15 +2,19 @@ package controllers
 
 import (
 	"bit_score/dto"
+	"bit_score/usecases"
 
 	"github.com/gin-gonic/gin"
 )
 
 type SessionsController struct {
+	useCase usecases.SessionUseCase
 }
 
-func NewSessionsController() *SessionsController {
-	return &SessionsController{}
+func NewSessionsController(useCase usecases.SessionUseCase) *SessionsController {
+	return &SessionsController{
+		useCase: useCase,
+	}
 }
 
 func (sc *SessionsController) CreateSession(c *gin.Context) {
@@ -20,4 +24,17 @@ func (sc *SessionsController) CreateSession(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+
+	user, err := sc.useCase.Login(session.UserName, session.Password)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to login"})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Login successful",
+		"user":    user,
+		"token":   "dummy_token",
+	})
 }
