43d2db508c76717bf622e062ecfd9729df99fb38
Diego <diegosantos4957@gmail.com>
2025-08-24 16:36:55 -0300

    feat: update PlayerController to use PlayerUseCase for player creation

--- controllers/players.controller.go
+++ controllers/players.controller.go
@@ -2,15 +2,19 @@ package controllers
 
 import (
 	"bit_score/dto"
+	"bit_score/usecases"
 
 	"github.com/gin-gonic/gin"
 )
 
 type PlayerController struct {
+	playerUseCase usecases.PlayerUseCase
 }
 
-func NewSessionController() *PlayerController {
-	return &PlayerController{}
+func NewPlayerController(useCase usecases.PlayerUseCase) *PlayerController {
+	return &PlayerController{
+		playerUseCase: useCase,
+	}
 }
 
 func (sc *PlayerController) CreatePlayer(c *gin.Context) {
@@ -20,4 +24,15 @@ func (sc *PlayerController) CreatePlayer(c *gin.Context) {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+
+	err := sc.playerUseCase.CreatePlayer(player.Name)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to create player"})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Player created successfully",
+		"player":  player,
+	})
 }
