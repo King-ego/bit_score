10e7df8d31661561f46423157b4538f871cb39e9
Diego <diegosantos4957@gmail.com>
2025-08-24 16:36:46 -0300

    feat: implement PlayerUseCase with CreatePlayer method for player creation

--- usecases/player.usecase.go
+++ usecases/player.usecase.go
@@ -2,6 +2,21 @@ package usecases
 
 import "bit_score/repositories"
 
-type LoginRequest struct {
+type PlayerUseCase struct {
 	repository repositories.PlayersRepository
+}
+
+func NewPlayerRequest(repository repositories.PlayersRepository) PlayerUseCase {
+	return &PlayerUseCase{repository: repository}
+}
+
+func (p *PlayerUseCase) CreatePlayer(name string) error {
+	player := repositories.ICreatePlayer{
+		Name: name,
+	}
+	err := p.repository.Create(player)
+	if err != nil {
+		return err
+	}
+	return nil
 }
